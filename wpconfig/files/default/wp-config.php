9d507482dd9ebfac016718fe9d17d2ee31065c5d
yashmeena7005 <yashmeena26@gmail.com>
2015-11-15 23:02:51 +0530

    Update wp-config.php

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -14,22 +14,22 @@
  * @package WordPress
  */
 
-define('WP_REDIS_HOST', 'geekme.blfdhx.0001.usw2.cache.amazonaws.com');
+define('WP_REDIS_HOST', '');
  
 
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress_db');
+define('DB_NAME', '');
 
 /** MySQL database username */
-define('DB_USER', 'wordpress_db');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'rgtunet11');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'wordpress-db.caguwwu44voj.us-west-2.rds.amazonaws.com');
+define('DB_HOST', '');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
